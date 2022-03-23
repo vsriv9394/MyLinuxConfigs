b1eba216e627b2b68772aac956e43443c049fc8e
vsriv9394 <vsriv@umich.edu>
2022-03-23 16:30:37 -0400

    Commented out the double initialization

--- configs/nvim/lua/lsp.lua
+++ configs/nvim/lua/lsp.lua
@@ -72,15 +72,15 @@ cmp.setup.cmdline(':', {
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['clangd'].setup {
-    capabilities = capabilities
-}
-require('lspconfig')['pyright'].setup {
-    capabilities = capabilities
-}
-require('lspconfig')['texlab'].setup {
-    capabilities = capabilities
-}
+-- require('lspconfig')['clangd'].setup {
+--     capabilities = capabilities
+-- }
+-- require('lspconfig')['pyright'].setup {
+--     capabilities = capabilities
+-- }
+-- require('lspconfig')['texlab'].setup {
+--     capabilities = capabilities
+-- }
 --require('lspconfig')['pylsp'].setup {
 --    capabilities = capabilities
 --}
