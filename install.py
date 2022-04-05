817dc13d4d6411725a05cd4c758fb7d865dbd4b2
vsriv9394 <vsriv@umich.edu>
2022-04-05 15:55:34 -0400

    Added optional installation of neovim'

--- install.py
+++ install.py
@@ -4,10 +4,23 @@ from INSTALL.nvim import installConfig_nvim
 from INSTALL.shell import installConfig_shell
 from INSTALL.externals import installExternals
 
-installExternals('x86_64')
+def getOption(question, validResponseList):
+    question += ' (' + '/'.join(validResponseList) + ')'
+    question += ' [' + validResponseList[0] + ']'
+    response = ''
+    while response not in validResponseList:
+        response = input(question)
+        if response=='': response = validResponseList[0]
+        if response not in validResponseList: print('Invalid Option')
+        else: return response
 
 installConfig_shell()
 installConfig_basic()
-installConfig_nvim()
+
+neovimNeeded = getOption('Do you wish to install neovim and associated packages?', ['n', 'y'])
+if neovimNeeded == 'y':
+    platform = getOption('Please provide the hardware platform being used', ['x86_64', 'ppc64le'])
+    installExternals(platform)
+    installConfig_nvim()
 
 install_gitsync()
